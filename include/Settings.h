8cb6d6c5a0c0e07a51754360abd7b6e5c4f5bed2
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-12-20 16:26:53 +0100

    Update Settings.h

--- include/Settings.h
+++ include/Settings.h
@@ -4,7 +4,7 @@ namespace Settings {
     inline std::string mod_name = "Dynamic Tooltips";
     inline bool disallow_editorIDs = false;
     inline bool show_titles = true;
-    inline bool show_spell_levels = false;
+    inline bool show_spell_levels = true;
 
     void Load();
     void Save();
